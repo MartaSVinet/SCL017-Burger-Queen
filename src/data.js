b9abe06c78043386c6f1c76b0d433bed50bc2626
carlamorales <carlamoralespacheco@gmail.com>
2021-10-03 17:30:58 -0300

    se crea archivo data y se renombra fichero con imágenes

---
+++ src/data.js
@@ -0,0 +1,96 @@
+const data = {
+  breakfasts: [
+    {
+      id: '1',
+      name: 'Café',
+      price: 1000,
+      image: './images/breakfasts/cafe.png',
+    },
+    {
+      id: '2',
+      name: 'Café c/leche',
+      price: 1500,
+      image: './images/breakfasts/cafeconleche.png',
+    },
+    {
+      id: '3',
+      name: 'Sandwich J&Q',
+      price: 1200,
+      image: './images/breakfasts/sandwichjq.png',
+    },
+    {
+      id: '4',
+      name: 'Jugo de frutas',
+      price: 1000,
+      image: './images/breakfasts/jugodefrutas.png',
+    },
+    {
+      id: '5',
+      name: 'Tutti frutti',
+      price: 2500,
+      image: './images/breakfasts/tuttifrutti.png',
+    },
+    {
+      id: '6',
+      name: 'Leche c/cereales',
+      price: 1500,
+      image: './images/breakfasts/lecheconcereal.png',
+    },
+  ],
+  burgers: [
+    {
+      id: '7',
+      name: 'Hamburguesa simple',
+      price: 3000,
+      image: './images/burgers/simple.png',
+    },
+    {
+      id: '8',
+      name: 'Hamburguesa doble',
+      price: 5000,
+      image: './images/burgers/doble.png',
+    },
+  ],
+  sidedishes: [
+    {
+      id: '9',
+      name: 'Papas fritas',
+      price: 2500,
+      image: './images/sidedishes/papitas.png',
+    },
+    {
+      id: '10',
+      name: 'Aros de cebolla',
+      price: 2500,
+      image: './images/sidedishes/arosdecebolla.png',
+    },
+  ],
+  drinks: [
+    {
+      id: '11',
+      name: 'Agua 500ml',
+      price: 500,
+      image: './images/drinks/aguachica.png',
+    },
+    {
+      id: '12',
+      name: 'Agua 750ml',
+      price: 700,
+      image: './images/drinks/aguagrande.png',
+    },
+    {
+      id: '13',
+      name: 'Gaseosa 500ml',
+      price: 700,
+      image: './images/drinks/bebidachica.png',
+    },
+    {
+      id: '14',
+      name: 'Gaseosa 750ml',
+      price: 1000,
+      image: './images/drinks/bebidagrande.png',
+    },
+  ],
+};
+
+export default data;
